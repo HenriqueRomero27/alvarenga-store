06a9bd1864fc1694e6fd29b2bef38ccef5ab1beb
HenriqueRomero27 <luishenriquebuenoromero@gmail.com>
2022-12-01 09:11:45 -0300

    init project

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var bt1 = 0, bt2 = 0, bt3 = 0, bt4 = 0, bt5 = 0, bt6 = 0, bt7 = 0, bt8 = 0;
+
+function processar() {
+    var count = document.getElementById('count')
+    count.innerHTML = parseInt(count.innerText) + 1
+}
